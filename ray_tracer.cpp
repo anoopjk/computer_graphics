cbc8e946ec93b8d1a279f6b394a37397ddc70abe
anoopjk <anoopnwu@gmail.com>
2016-01-16 19:13:42 -0800

    Create ray_tracer.cpp

---
+++ ray_tracer.cpp
@@ -0,0 +1,143 @@
+/**
+ * ray_tracer.cpp
+ * CS230
+ * -------------------------------
+ * Implement ray tracer here.
+ */
+
+#define SET_RED(P, C)   (P = (((P) & 0x00ffffff) | ((C) << 24)))
+#define SET_GREEN(P, C)  (P = (((P) & 0xff00ffff) | ((C) << 16)))
+#define SET_BLUE(P, C) (P = (((P) & 0xffff00ff) | ((C) << 8)))
+
+#include "ray_tracer.h"
+
+using namespace std;
+
+const double Object::small_t=1e-6;
+//--------------------------------------------------------------------------------
+// utility functions
+//--------------------------------------------------------------------------------
+double sqr(const double x)
+{
+    return x*x;
+}
+
+Pixel Pixel_Color(const Vector_3D<double>& color)
+{
+    Pixel pixel=0;
+    SET_RED(pixel,(unsigned char)(min(color.x,1.0)*255));
+    SET_GREEN(pixel,(unsigned char)(min(color.y,1.0)*255));
+    SET_BLUE(pixel,(unsigned char)(min(color.z,1.0)*255));
+    return pixel;
+}
+//--------------------------------------------------------------------------------
+// Shader
+//--------------------------------------------------------------------------------
+Vector_3D<double> Phong_Shader::
+Shade_Surface(const Ray& ray,const Object& intersection_object,const Vector_3D<double>& intersection_point,const Vector_3D<double>& same_side_normal) const
+{
+    Vector_3D<double> color;
+
+    // TODO: determine the color
+
+    return color;
+}
+
+Vector_3D<double> Reflective_Shader::
+Shade_Surface(const Ray& ray,const Object& intersection_object,const Vector_3D<double>& intersection_point,const Vector_3D<double>& same_side_normal) const
+{
+    Vector_3D<double> color;
+
+    // TODO: determine the color
+
+    return color;
+}
+
+Vector_3D<double> Flat_Shader::
+Shade_Surface(const Ray& ray,const Object& intersection_object,const Vector_3D<double>& intersection_point,const Vector_3D<double>& same_side_normal) const
+{
+    return color;
+}
+
+//--------------------------------------------------------------------------------
+// Objects
+//--------------------------------------------------------------------------------
+// determine if the ray intersects with the sphere
+// if there is an intersection, set t_max, current_object, and semi_infinite as appropriate and return true
+bool Sphere::
+Intersection(Ray& ray) const
+{
+    // TODO                       //intersection implies b^2 - 4*a*c >=0
+    return false;
+}
+
+Vector_3D<double> Sphere::
+Normal(const Vector_3D<double>& location) const
+{
+    Vector_3D<double> normal;
+    // TODO: set the normal
+    return normal;
+}
+
+// determine if the ray intersects with the plane
+// if there is an intersection, set t_max, current_object, and semi_infinite as appropriate and return true
+bool Plane::
+Intersection(Ray& ray) const
+{
+    // TODO
+    return false;
+}
+
+Vector_3D<double> Plane::
+Normal(const Vector_3D<double>& location) const
+{
+    return normal;
+}
+//--------------------------------------------------------------------------------
+// Camera
+//--------------------------------------------------------------------------------
+// Find the world position of the input pixel
+Vector_3D<double> Camera::
+World_Position(const Vector_2D<int>& pixel_index)
+{
+    Vector_3D<double> result;
+    // TODO 
+    return result;
+}
+//--------------------------------------------------------------------------------
+// Render_World
+//--------------------------------------------------------------------------------
+// Find the closest object of intersection and return a pointer to it
+//   if the ray intersects with an object, then ray.t_max, ray.current_object, and ray.semi_infinite will be set appropriately
+//   if there is no intersection do not modify the ray and return 0
+const Object* Render_World::
+Closest_Intersection(Ray& ray)
+{
+    // TODO
+    return 0;
+}
+
+// set up the initial view ray and call 
+void Render_World::
+Render_Pixel(const Vector_2D<int>& pixel_index)
+{
+    // TODO
+    Ray ray; // TODO: set up the initial view ray here
+
+    Ray dummy_root;
+    Vector_3D<double> color=Cast_Ray(ray,dummy_root);
+    camera.film.Set_Pixel(pixel_index,Pixel_Color(color));
+}
+
+// cast ray and return the color of the closest intersected surface point, 
+// or the background color if there is no object intersection
+Vector_3D<double> Render_World::
+Cast_Ray(Ray& ray,const Ray& parent_ray)
+{
+    // TODO
+    Vector_3D<double> color;
+
+    // determine the color here
+
+    return color;
+}
